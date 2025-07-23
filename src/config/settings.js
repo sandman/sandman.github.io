ff1e8a88e61c05b124633b2eb7cae50cf4e6ef1f
Sandip Gangakhedkar <sandip.gangakhedkar@gmail.com>
2025-07-23 22:21:18 +0100

    Advanced SEO with schema.org markup

--- src/config/settings.js
+++ src/config/settings.js
@@ -1,6 +1,6 @@
 export default {
-	title: `Sandip Gangakhedkar | Fractional CTO in London`,
-	description: `London-based factional CTO/CPO and technical advisor for early-stage startups and growing teams. `,
+	title: `Sandip Gangakhedkar | Fractional CTO/CPO in London`,
+	description: `London-based CTO/CPO and technical advisor for early-stage startups and growing teams. `,
 	url: `https://sandip.live`, // No trailing slash!
 	name: `Sandip Gangakhedkar`, // The short name of the business or brand name. Used for things like the copyright in the footer.
 	enableThemeSwitcher: false,
